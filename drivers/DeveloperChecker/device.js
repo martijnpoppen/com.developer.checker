423d1f8807b2208efaea3e1febc818b82e3219d4
Martijn Poppen <m.poppen@ziggo.nl>
2021-10-17 22:31:41 +0200

    NEW: DeveloperChecker

---
+++ drivers/DeveloperChecker/device.js
@@ -0,0 +1,131 @@
+const Homey = require('homey');
+const API = require('../../lib/homey');
+const { sleep } = require('../../lib/helpers');
+
+module.exports = class DeveloperChecker extends Homey.Device {
+    async onInit() {
+        const settings = this.getSettings();
+
+        await this.checkCapabilities();
+
+        this.homey.app.log('[onInit] - Loaded settings', settings);
+
+        this._apiClient = await new API(settings);
+
+        if (!settings.AUTH && !settings.AUTH.acces_token) {
+            const auth = this._apiClient.getBearer();
+            await this.setSettings({ ...settings, AUTH: auth });
+        }
+
+        await this.InitFinder(true);
+        this.setAvailable();
+    }
+
+    async checkCapabilities() {
+        const driverManifest = this.driver.manifest;
+        const driverCapabilities = driverManifest.capabilities;
+
+        const deviceCapabilities = this.getCapabilities();
+
+        this.homey.app.log(`[Device] ${this.getName()} - Found capabilities =>`, deviceCapabilities);
+        this.homey.app.log(`[Device] ${this.getName()} - Driver capabilities =>`, driverCapabilities);
+
+        if (deviceCapabilities.length !== driverCapabilities.length) {
+            await this.updateCapabilities(driverCapabilities, deviceCapabilities);
+        }
+
+        return deviceCapabilities;
+    }
+
+    async updateCapabilities(driverCapabilities, deviceCapabilities) {
+        this.homey.app.log(`[Device] ${this.getName()} - Add new capabilities =>`, driverCapabilities);
+        try {
+            deviceCapabilities.forEach((c) => {
+                this.removeCapability(c);
+            });
+            await sleep(2000);
+            driverCapabilities.forEach((c) => {
+                this.addCapability(c);
+            });
+            await sleep(2000);
+        } catch (error) {
+            this.homey.app.log(error);
+        }
+    }
+
+    async setInitFinderInterval(clear = false) {
+        const REFRESH_INTERVAL = 1000 * (0.5 * 60);
+
+        if (clear) {
+            this.homey.app.log(`[onPollInterval] - Clearinterval`);
+            this.homey.clearInterval(this.onPollInterval);
+        }
+
+        this.homey.app.log(`[onPollInterval]`, REFRESH_INTERVAL);
+        this.onPollInterval = this.homey.setInterval(this.InitFinder.bind(this), REFRESH_INTERVAL);
+    }
+
+    async InitFinder(initial = false, force = false) {
+        try {
+            await this.findApps();
+
+            if (initial) {
+                // await sleep(9000);
+                await this.setInitFinderInterval();
+            }
+        } catch (error) {
+            this.error(error);
+        }
+    }
+
+    async findApps() {
+        const settings = this.getSettings();
+        const appArray = settings.APPS;
+
+        this.homey.app.log(`[findapps] - appArray: `, appArray);
+
+        let apps = await this._apiClient.getApps(settings.AUTH);
+
+        if (!apps.data) return;
+        const auth = apps.auth;
+        apps = Object.values(apps.data).map((f) => ({ name: f.liveBuild.name.en, id: f.id, installs: f.installs }));
+
+        let totalInstalls = 0;
+
+        apps.forEach((app) => {
+            totalInstalls = totalInstalls + app.installs;
+        });
+
+        await this.setSettings({ ...settings, APPS: [...new Set(apps)], AUTH: auth });
+        await this.setCapabilityValue('measure_installs', totalInstalls);
+        await this.checkAppDiff(apps, appArray);
+    }
+
+    async checkAppDiff(apps, appArray) {
+        try {
+            const appDiff = apps.filter((a, index) => !!appArray[index] && a.installs > appArray[index].installs);
+            const appDiffReverse = appArray.filter((a, index) => !!apps[index] && a.installs > apps[index].installs);
+
+            this.homey.app.log(`[appDiff] - appDiff: `, appDiff);
+            this.homey.app.log(`[appDiffReverse] - appDiffReverse: `, appDiffReverse);
+
+            appDiff.forEach(async (app) => {
+                await this.homey.app
+                    .getDeviceTriggerCard(`trigger_INSTALL_ADD`)
+                    .trigger({ app: app.name, id: app.id, install: app.installs })
+                    .catch(this.error)
+                    .then(this.homey.app.log(`[appDiff] trigger_INSTALL_ADD - Triggered: "${app.name} | ${app.id} | ${app.installs}"`));
+            });
+
+            appDiffReverse.forEach(async (app) => {
+                await this.homey.app
+                    .getDeviceTriggerCard(`trigger_INSTALL_REMOVE`)
+                    .trigger({ app: app.name, id: app.id, install: app.installs })
+                    .catch(this.error)
+                    .then(this.homey.app.log(`[appDiff] trigger_INSTALL_REMOVE - Triggered: "${app.name} | ${app.id} | ${app.installs}"`));
+            });
+        } catch (error) {
+            this.error(error);
+        }
+    }
+};
---
+++ drivers/DeveloperChecker/driver.js
@@ -0,0 +1,59 @@
+const Homey = require('homey');
+const API = require('../../lib/homey');
+
+module.exports = class DeveloperChecker extends Homey.Driver {
+    onInit() {
+        this.homey.app.log('[Driver] - init', this.id);
+        this.homey.app.log(`[Driver] - version`, Homey.manifest.version);
+    }
+
+    async onPair(session) {
+        session.setHandler("login", async (data) => {
+            try {
+                this.config = {
+
+                };
+                this.config = {
+                    ...Homey.env,
+                    'email': data.username,
+                    'password': data.password,
+                    'otp': data.otp
+                }
+
+                this.homey.app.log(`[Driver] ${this.id} - got config`, this.config);
+            
+                this._apiClient = await new API(this.config);
+                
+                this.homeydata = await this._apiClient._getBearer({});
+            } catch (error) {
+                throw new Error(this.homey.__('pair.error'));
+            }
+        });
+
+        session.setHandler("list_devices", async () => {
+            let results = [];
+
+            this.homey.app.log(`[Driver] ${this.id} - this.homeydata`, this.homeydata);
+
+            if(!this.homeydata || !this.homeydata.data && !this.homeydata.data.token_type) {
+                throw new Error(this.homey.__('pair.error'));
+            }
+
+            results.push({
+                name: `Developer Checker - ${this.config.email}`,
+                data: {
+                    id: `${this.id}-${this.config.email}`,
+                },
+                settings: {
+                    ...this.config,
+                    AUTH: this.homeydata.data,
+                    APPS: []
+                }
+            });
+
+            this.homey.app.log(`[Driver] ${this.id} - Found devices - `, results);
+
+            return results;
+        });
+    }
+}
