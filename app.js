0901746c364ee43d8d507484185c6ff43ac55cad
Martijn Poppen <m.poppen@ziggo.nl>
2021-10-20 21:25:46 +0200

    OPT: app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const Homey = require("homey");
+const { encrypt } = require('./lib/helpers')
+
+class App extends Homey.App {
+  log() {
+    console.log.bind(this, "[log]").apply(this, arguments);
+  }
+
+  error() {
+    console.error.bind(this, "[error]").apply(this, arguments);
+  }
+
+  // -------------------- INIT ----------------------
+
+  async onInit() {
+    this.log(`${this.homey.manifest.id} - ${this.homey.manifest.version} started...`);
+  }
+}
+
+module.exports = App;
